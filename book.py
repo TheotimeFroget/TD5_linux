06f8cc2a920c3793fa3034096e6e56e6170ab9c9
TheotimeF <61406081+TheotimeFroget@users.noreply.github.com>
2022-03-22 14:45:47 +0100

    Add files via upload

--- book.py
+++ book.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 21 23:02:39 2022
+
+@author: louis
+"""
+
 class Book:
     def __init__(self, name):
         self.name = name
@@ -78,3 +85,17 @@ class Order:
             self.type = type
             self.id = id
 
+
+#from book import Book
+
+def main():
+    book = Book("TEST")
+    book.insert_buy(10, 10.0)
+    book.insert_sell(120, 12.0)
+    book.insert_buy(5, 10.0)
+    book.insert_buy(2, 11.0)
+    book.insert_sell(1, 10.0)
+    book.insert_sell(10, 10.0)
+
+if __name__ == "__main__":
+    main()
